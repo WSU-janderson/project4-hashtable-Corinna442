dad33c2a287c1be9f0331abfb9da306799a7b1dc
Corinna442 <green.442@wright.edu>
2025-11-01 20:55:07 -0400

    Set up public and private methods and variables in .h. Also include Entry structure.

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,3 +1,41 @@
 /**
  * HashTable.cpp
  */
+
+#include "HashTable.h"
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+
+/**
+ * A single constructor that takes an initial capacity for the table. If no
+ * capacity is given, then it defaults to 8 initially.
+ *
+ */
+HashTable::HashTable(size_t initCapacity) {
+
+}
+
+/**
+ * Insert a new key-value pair into the table. Duplicate keys are not allowed.
+ * The method should return true if the insertion was successful. If the insertion was
+ * unsuccessful, such as when a duplicate is attempted to be inserted, the method
+ * should return false.
+ */
+bool HashTable::insert(const std::string& key, const size_t& value) {
+
+}
+
+/**
+ * If the key is in the table, remove will "erase" the key-value pair from
+ * the table. This might just be marking a bucket as empty-after-remove
+ */
+bool HashTable::remove(const std::string& key) {}
+
+/**
+ * returns true if the key is in the table and false if the key is not in the table
+ */
+bool HashTable::contains(const std::string& key) const {
+
+}
--- HashTable.h
+++ HashTable.h
@@ -1,3 +1,47 @@
 /**
  * HashTable.h
  */
+
+#ifndef HASH_TABLE_H
+#define HASH_TABLE_H
+
+#include <string>
+#include <cstddef> // For size_t
+#include <stdexcept> // exceptions
+#include <vector>
+
+class HashTable {
+    public:
+
+    HashTable(size_t initCapacity = 8);
+    bool insert(const std::string& key, const size_t& value);
+    bool remove(const std::string& key);
+    bool contains(const std::string& key) const;
+
+    private:
+
+    struct Entry {
+        std::string key;
+        size_t value;
+        bool occupied = false;
+        bool deleted = false;
+    };
+
+    std::vector<Entry> table;
+    size_t capacity;
+    size_t count;
+
+    // first index where key should go
+    size_t hash(const std::string& key) const;
+
+    // step size for psuedo-random probing
+    size_t secondHash(const std::string& key) const;
+
+    // Run probing to locate index where the key should be stored
+    //size_t findInsertIndex(const std::string& key) const;
+
+
+
+};
+
+#endif
